6b6733ff9fef5351ac5fd6f9f9b5e43fb1ed1640
Tracy Sonyah <sonyahtracy@gmail.com>
2023-06-04 17:01:15 +0000

    Not done yet

---
+++ conditions.js
@@ -0,0 +1,65 @@
+//problem set 5
+var number = parseInt(prompt('Enter a number '));
+
+if (number % 3 === 0 && number % 5 === 0) {
+    console.log('FIZZBUZZ')
+}
+
+else if (number % 3 === 0) {
+    console.log('FIZZ');
+}
+
+else if (number % 5 === 0) {
+    console.log('BUZZ');
+}
+
+else {
+    console.log(number)
+}
+
+
+
+//problem set 4
+var num = +prompt('Enter a number pls ');
+
+
+switch(num) {
+    case 0:        
+    case 1:
+        console.log(1);
+    break;
+
+    default:
+        console.log(num * (num - 1) * (n - 2) * '...' * 3 * 2 * 1)
+}
+
+
+//problem set 3
+var num = parseInt(prompt('Insert a num'))
+
+if (num % 2 === 0) {
+    console.log('This is an even number' );
+}
+
+else if (num % 2 !== 0) {
+    console.log('This is an odd number')
+}
+
+else {
+    console.log('Tracy, u can do better')
+}
+
+
+//problem set 2
+var num = +prompt('Enter any number ')
+var factor = 2
+
+if (num % factor === 0 && factor % i === 0) {
+    console.log()
+}
+
+
+//problem set 1
+var num1 = parseInt(prompt('Insert a number '));
+var num2 = parseInt(prompt('Insert another number '));
+
